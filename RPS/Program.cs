6f6a0868eba678163736c4d3d9cf6dd2f5c5055b
Dipperra <kt1luarrackzz1xr0s@gmail.com>
2024-11-27 21:06:56 +0300

    version 2

--- RPS/Program.cs
+++ RPS/Program.cs
@@ -5,15 +5,17 @@ class Program
     public const string Paper = "бумага";
     public const string Rock = "камень";
     public const string Scissors = "ножницы";
+
     static void Main()
     {
         Console.WriteLine("Введите 'камень', 'ножницы' или 'бумага' для игры.");
         Console.WriteLine("Введите 'стоп' для завершения игры.");
         Random random = new Random();
+        Console.WriteLine("Ваш выбор: ");
+
         while (true)
         {
-            Console.WriteLine("Ваш выбор: ");
-            string userInput = Console.ReadLine();
+            string userInput = Console.ReadLine().ToLower();
 
             if (userInput == "стоп")
             {
@@ -24,33 +26,40 @@ class Program
                 Console.WriteLine("Некорректный ввод. Пожалуйста, попробуйте еще раз.");
                 continue;
             }
-            int Choice = random.Next(1, 4);
-            string PSChoice = "";
-            if (Choice == 1)
-            {
-                PSChoice = Rock;
-            }
-            else if (Choice == 2)
-            {
-                PSChoice = Paper;
-            }
-            else if (Choice == 3)
-            {
-                PSChoice = Scissors;
-            }
+
+            string PSChoice = GetpsChoice(random);
             Console.WriteLine($"Выбор оппонента: {PSChoice}");
-            if (PSChoice == userInput)
-            {
-                Console.WriteLine("ничья");
-            }
-            else if ((userInput == Rock && PSChoice == Scissors) || (userInput == Paper && PSChoice == Rock) || (userInput == Scissors && PSChoice == Paper))
-            {
-                Console.WriteLine("Вы выиграли");
-            }
-            else
-            {
-                Console.WriteLine("Вы проиграли");
-            }
+            string result = GetResult(userInput, PSChoice);
+            Console.WriteLine(result);
+        }
+    }
+
+    static string GetpsChoice(Random random)
+    {
+        int choice = random.Next(1, 4);
+        string psChoice = choice switch
+        {
+            1 => Rock,
+            2 => Paper,
+            3 => Scissors
+        };
+
+        return psChoice;
+    }
+
+    static string GetResult(string userChoice, string psChoice)
+    {
+        if (userChoice == psChoice)
+        {
+            return "ничья";
+        }
+        else if ((userChoice == Rock && psChoice == Scissors) || (userChoice == Paper && psChoice == Rock) || (userChoice == Scissors && psChoice == Paper))
+        {
+            return "Вы выиграли";
+        }
+        else
+        {
+            return "Вы проиграли";
         }
     }
 }
